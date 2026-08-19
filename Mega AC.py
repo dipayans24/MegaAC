a6a57b0fc8d00808f3e6136e0dfcc74b9680c2c4
dipayans24 <dipayans24@yahoo.com>
2026-08-19 14:54:28 +0530

    Update Mega AC.py

--- Mega AC.py
+++ Mega AC.py
@@ -578,6 +578,7 @@ if WSDate and MetaACData and GdriveCredentials and credential_Upload:
 
 else:
     with st.status("Credentials", expanded=False):
+        st.code("https://metabase-production-6ca8.up.railway.app/", language="html")
         st.code("rezaul.alam@houseofedtech.in")
         st.code("FgbBpmOfK68FBt")
         
