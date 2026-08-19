56ecf48d9aa9d5f627dcff9e3934d25cfdc64d9f
dipayans24 <dipayans24@yahoo.com>
2026-08-19 14:48:02 +0530

    Update Mega AC.py

--- Mega AC.py
+++ Mega AC.py
@@ -582,4 +582,7 @@ else:
         FunnelCheckBox = st.selectbox("Select a Funnel to get code", FunnelsList)
         st.code(generate_code(FunnelCheckBox, WSDate), language="sql")
 
+        st.code("rezaul.alam@houseofedtech.in")
+        st.code("FgbBpmOfK68FBt")
+
 
